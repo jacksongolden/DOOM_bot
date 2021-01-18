f844127cd702d5b671b286758a6b56ecb7267b97
Jackson Golden <jackson.golden@gmail.com>
2021-01-18 16:23:42 -0500

    Defined DOOMBot class.
    Will migrate code to this class in future...

---
+++ DOOM_bot.py
@@ -0,0 +1,6 @@
+class DOOMBot:
+    """
+    Uses the lyricsgenius package and genius.com API to generate a tweet of random MF DOOM lyrics
+    with associated info (track name, album name, etc.)
+    Uses tweepy package to post tweet.
+    """
